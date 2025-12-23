7b217e6b0bb60d19ad3bb7f8718e14b2f0d1bbd7
alona-gre <alona.hrechka@regnosys.com>
2025-12-23 19:57:03 +0000

    fix landing page content

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -1,5 +1,6 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
+import Link from '@docusaurus/Link';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
@@ -7,49 +8,52 @@ type FeatureItem = {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: ReactNode;
+  link?: string;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Keyword search',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Search for a keyword to find the modelling component info you want, fast.
       </>
     ),
+    link: '/docs/rune-documentation/overview/keyword-search',
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Components',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Our modelling components and how they work, with examples to show each feature.
       </>
     ),
+    link: '/docs/rune-documentation/rune-dsl-modelling-components',
   },
   {
-    title: 'Powered by React',
+    title: 'Get Started',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        New to Rune? Interested in data modelling? Start here to find out all you need to know
       </>
     ),
+    link: '/docs/rune-documentation/overview/get-started',
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description, link }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+        <Heading as="h3">
+          {link ? <Link to={link}>{title}</Link> : title}
+        </Heading>
         <p>{description}</p>
       </div>
     </div>
