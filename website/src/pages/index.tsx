414a08efc44b88eb9bab0f185ac12cb44213cb18
Chuks-Ajeh <chuks.ajeh@regnosys.com>
2026-02-12 09:13:39 +0000

    task-8185: update rune docs button page link

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,7 +34,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/docs/get-started">
+            to="/docs/introducing-rune">
             Rune Docs
           </Link>
           <Link
